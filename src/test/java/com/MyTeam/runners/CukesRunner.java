43913d6854bd32bcfdcc592771f18680f892375a
Erkan <ErkanOzulu@users.noreply.github.com>
2024-01-27 16:56:07 +0300

    tag

--- src/test/java/com/MyTeam/runners/CukesRunner.java
+++ src/test/java/com/MyTeam/runners/CukesRunner.java
@@ -16,7 +16,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue="com/MyTeam/step_definetions",
         dryRun = false,
-        tags="@login",
+        tags="",
         publish=true
 
 
